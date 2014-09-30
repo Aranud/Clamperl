f55189f700434f39280480f28d7544066b8f83ae
Aranud <aranud66@live.fr>
2014-09-30 16:59:07 +0200

    Revert "Revert "Getter & Setters""
    This reverts commit 257c3923b7da01e05f0873e55ecae14bd0e5bc60.

--- ApplicationServer/src/main/java/Classes/Country.java
+++ ApplicationServer/src/main/java/Classes/Country.java
@@ -1,12 +1,29 @@
 package Classes;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by imerir on 30/09/2014.
  */
-public class Country {
+public class Country implements Serializable{
 
     private String name;
     private List<State> states;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
 }
